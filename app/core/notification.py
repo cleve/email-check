90e4d3b8a57984b4b4061a84c9be83871c06aa15
Mauricio Cleveland <mauricio.cleveland@gmail.com>
2022-05-12 18:43:46 -0400

    Improves

--- app/core/notification.py
+++ app/core/notification.py
@@ -5,7 +5,8 @@ class Notification:
     def send_notification(messages: list) -> None:
         if len(messages) <= 3:
             for message in messages:
-                subprocess.call(['notify-send', message.title, message.body])
+                subprocess.call(
+                    ['notify-send', f'New e-mail from {message.title}', message.body])
         else:
             subprocess.call(['notify-send', "New emails"])
 
