b2cb2f6556368c54e01633d9fb3cf5260e59ec68
Mauricio Cleveland <mauricio.cleveland@gmail.com>
2022-05-13 20:19:56 -0400

    Notification

--- app/core/notification.py
+++ app/core/notification.py
@@ -3,7 +3,7 @@ import subprocess
 class Notification:
     @staticmethod
     def send_notification(messages: list) -> None:
-        if len(messages) <= 3:
+        if len(messages) == 1:
             for message in messages:
                 subprocess.call(
                     ['notify-send', f'New e-mail from {message.title}', message.body])
